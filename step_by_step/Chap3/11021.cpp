5a06359b562d40199e1d87f82e5e1ad9bf34fab0
hyeon7427@gmail.com <hyeon7427@gmail.com>
2020-11-06 21:06:49 +0900

    11021.cpp finished in 20.11.06

---
+++ step_by_step/Chap3/11021.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int num,a,b;
+
+
+    cin>>num;
+
+    int arr[num];
+
+    
+    for(int i=0;i<num;i++)
+    {
+      cin>>a;
+      cin>>b;
+
+      arr[i]=a+b;
+    }
+
+    for(int i=0;i<num;i++)
+    {
+        cout<<"Case #"<<i+1<<": "<<arr[i]<<'\n';
+    }
+
+    return 0;
+}
