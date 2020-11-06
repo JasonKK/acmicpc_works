fee7bfd7fb2e978976f568ed0a35749b7ae693bb
hyeon7427@gmail.com <hyeon7427@gmail.com>
2020-11-06 21:16:22 +0900

    2438.cpp finished in 20.11.05

---
+++ step_by_step/Chap3/2438.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int num;
+
+
+    cin>>num;
+
+    for(int i=0;i<num;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<'\n';
+    }
+}
