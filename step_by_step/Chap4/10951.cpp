9a5cecfc95badcb4b7eebf1dec0b1f1b80c9a22f
hyeon7427@gmail.com <hyeon7427@gmail.com>
2020-11-07 22:22:17 +0900

    10951.cpp finished in 20.11.07

---
+++ step_by_step/Chap4/10951.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int a,b;
+
+    while(cin>>a>>b)
+    {
+        cout<<a+b<<endl;
+    }
+
+    return 0;
+}
